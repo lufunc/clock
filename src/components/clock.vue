3d4cd2d7246ff68ec8e9f47f05decc23631ae2e1
LU <lh_aaa@qq.com>
2021-01-18 09:56:01 +0800

    feat:clock init

---
+++ src/components/clock.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="clock">
+    <div>{{ msg }}</div>
+    <halfClock></halfClock>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import halfClock from './halfClock'
+export default {
+  components: { halfClock },
+  setup () {
+    const msg = ref('clock')
+    return {
+      msg
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/components/halfClock.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="halfClock">
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup () {
+    const msg = ref('halfClock')
+    return {
+      msg
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
