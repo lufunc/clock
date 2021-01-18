3814e6938fafb71e66f86cb78c22ff000d78a3bd
LU <lh_aaa@qq.com>
2021-01-18 09:56:22 +0800

    feat:clock init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,49 @@
 <template>
-  <div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg">
+    <div>
+      <clock></clock>
+    </div>
+    <div>设置</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive, toRefs } from 'vue'
+import clock from './components/clock.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { clock },
+  setup () {
+    const data = reactive({
+      // clock content
+      timer1: {
+        ampm: false,
+        zero: false,
+        value: 0
+      },
+      timer2: {
+        ampm: false,
+        zero: false,
+        value: 0
+      },
+      // config
+      // 12h 24h 024h
+      hourFormat: 0,
+      scale: 1,
+      brightness: 1,
+      showBg: true
+    })
+    return {
+      ...toRefs(data)
+    }
   }
 }
 </script>
 
 <style lang="less">
-
+body{
+  margin: 0;
+}
+.bg{
+  background-color: burlywood;
+}
 </style>
