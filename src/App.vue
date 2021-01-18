e99650089c72c5b47377777f512e372ada774a5e
LU <lh_aaa@qq.com>
2021-01-18 23:49:53 +0800

    feat:setting

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,44 @@
         <clock :num="num3"></clock>
       </div>
     </div>
-    <div style="position: fixed;bottom: 8%;right: 8%;">
-      <div style="color:#fff;" @click="ttt">设置</div>
-      <div style="color:#fff;">{{num1}}</div>
+    <div class="set_box">
+      <div class="set_box_item">
+        <ul class="set_box_inner">
+          <li>
+            <div>Hour Format:</div>
+            <div>选择器</div>
+          </li>
+          <li>
+            <div>Scale:</div>
+            <div>进度条</div>
+          </li>
+          <li>
+            <div>Brightn:</div>
+            <div>进度条</div>
+          </li>
+          <li>
+            <div>Show</div>
+            <input type="checkbox" name="Background" id="Background" style="vertical-align: middle;">
+            <span>Background</span>
+          </li>
+        </ul>
+        <footer>@lupass 2021</footer>
+        <div>OK</div>
+      </div>
+      <img @click="ttt" class="setting" :src="pic_setting" alt="">
     </div>
   </div>
 </template>
 
 <script>
 import { reactive, toRefs, computed } from 'vue'
+import myPic from './assets/pic'
 import clock from './components/clock.vue'
 export default {
   components: { clock },
   setup () {
     const data = reactive({
+      pic_setting: myPic.pic_setting,
       // clock content
       num1: 11,
       num2: 0,
@@ -62,7 +86,7 @@ export default {
       data.num3 = zeroNum(res.s)
     }, 200);
     const ttt = () => {
-      console.log('设置ttt')
+      console.log('设置ttt',myPic)
       data.showSecond = !data.showSecond
     }
     return {
@@ -76,6 +100,11 @@ export default {
 <style lang="less">
 body{
   margin: 0;
+}
+ul{
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
 }
 .bg{
   position: fixed;
@@ -98,12 +127,61 @@ body{
   width: 47.6vw;
   height: 47.6vw;
   padding: 1vw;
-  font-size: 28vw;
+  font-size: 34vw;
 }
 .timer_inner3{
   box-sizing: border-box;
   width: 30.8vw;
   height: 30.8vw;
   font-size: 22vw;
+}
+.set_box{
+  position: fixed;
+  bottom: 8%;
+  right: 8%;
+  width: 200px;
+  height: 200px;
+  &:hover .setting{
+    opacity: 1;
+  }
+}
+.set_box_item{
+  // display: none;
+  position: absolute;
+  top: -170px;
+  left: -196px;
+  width: 430px;
+  height: 310px;
+  background-color: #262729;
+  border-radius: 20px;
+  border: 1px solid #666;
+  &::before{
+    content: '';
+    position: absolute;
+    bottom: 0;
+    right: 40px;
+    width: 24px;
+    height: 24px;
+    background-color: #262729;
+    border: 1px solid #666;
+    border-top-color: transparent;
+    border-left-color: transparent;
+    transform: translateY(50%) rotateZ(45deg);
+  }
+}
+.set_box_inner>li>div{
+  display: inline-block;
+}
+.setting{
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 36px;
+  height: 36px;
+  opacity: 0;
+  transition: all 0.5s ease-in-out;
+  &:hover{
+    transform: rotateZ(120deg);
+  }
 }
 </style>
